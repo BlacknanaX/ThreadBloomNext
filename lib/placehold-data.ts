24a965a96e8a5d2c843aabb4a5f9156a2ace3c8c
JoannaXu <joannaxu985@outlook.com>
2025-03-24 19:35:41 +0000

    solve placehold-data.ts products' image url

--- lib/placehold-data.ts
+++ lib/placehold-data.ts
@@ -11,49 +11,49 @@ export const Products = [
         id: "2",
         name: "Dragon Boat Puppy Dumpling - Sweetheart Edition",
         price: 15,
-        imageUrl: "#",
+        imageUrl: "/Granny Square Rabbit Charm - Vintage Crochet Pendant.png",
         category_id: "001"
     },
     {
         id: "3",
         name: "Running Shoes",
         price: 79.99,
-        imageUrl: "#",
+        imageUrl: "/Granny Square Rabbit Charm - Vintage Crochet Pendant.png",
         category_id: "001"
     },
     {
         id: "4",
         name: "Coffee Maker",
         price: 129.99,
-        imageUrl: "#",
+        imageUrl: "/Granny Square Rabbit Charm - Vintage Crochet Pendant.png",
         category_id: "001"
     },
     {
         id: "5",
         name: "Yoga Mat",
         price: 29.99,
-        imageUrl: "#",
+        imageUrl: "/Granny Square Rabbit Charm - Vintage Crochet Pendant.png",
         category_id: "001"
     },
     {
         id: "6",
         name: "Backpack",
         price: 49.99,
-        imageUrl: "#",
+        imageUrl: "/Granny Square Rabbit Charm - Vintage Crochet Pendant.png",
         category_id: "001"
     },
     {
         id: "7",
         name: "Blender",
         price: 89.99,
-        imageUrl: "#",
+        imageUrl: "/Granny Square Rabbit Charm - Vintage Crochet Pendant.png",
         category_id: "001"
     },
     {
         id: "8",
         name: "Sunglasses",
         price: 159.99,
-        imageUrl: "#",
+        imageUrl: "/Granny Square Rabbit Charm - Vintage Crochet Pendant.png",
         category_id: "001"
     }
 ];
