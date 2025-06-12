5c4f092ec0c7a3e8fde91f2b05cd2a4cf2939962
JoannaXu <joannaxu985@outlook.com>
2025-06-13 00:20:12 +0100

    customize only

--- app/dashboard/settings/menu/[id]/edit/page.tsx
+++
@@ -1,6 +0,0 @@
-export async function Page({params}:{
-    params: Promise<{id:string}>
-}){
-    const {id} = await params;
-    return <>{id}</>;
-}
--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // 移除实验性功能配置
-}
-
-module.exports = nextConfig 
